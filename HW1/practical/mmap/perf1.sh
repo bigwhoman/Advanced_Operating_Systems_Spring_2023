c163a421c92574f2f99404cdd7202b3c809de163
bigwhoman <hm.keshvari@gmail.com>
2023-04-11 18:06:12 +0330

    added some fixes to mmap

--- HW1/practical/mmap/perf1.sh
+++ HW1/practical/mmap/perf1.sh
@@ -1,6 +1,6 @@
-gcc ./protect.c -o myprogram -pg
-sudo perf stat -e faults,page-faults,dTLB-loads,dTLB-load-misses,iTLB-loads,iTLB-load-misses,context-switches ./myprogram
+gcc ./mmap.c -o mmap -pg
+sudo perf stat -e faults,page-faults,dTLB-loads,dTLB-load-misses,iTLB-loads,iTLB-load-misses,context-switches ./mmap
 sleep 1
-sudo perf stat -e faults,page-faults,dTLB-loads,dTLB-load-misses,iTLB-loads,iTLB-load-misses,context-switches ./myprogram D_write
+sudo perf stat -e faults,page-faults,dTLB-loads,dTLB-load-misses,iTLB-loads,iTLB-load-misses,context-switches ./mmap D_write
 sleep 1
-sudo perf stat -e faults,page-faults,dTLB-loads,dTLB-load-misses,iTLB-loads,iTLB-load-misses,context-switches ./myprogram C_write D_write
+sudo perf stat -e faults,page-faults,dTLB-loads,dTLB-load-misses,iTLB-loads,iTLB-load-misses,context-switches ./mmap C_write D_write
